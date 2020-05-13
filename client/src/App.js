002be6a71b3e03c50d90f0dd197eea59f677d324
Anirudh <anirudhganwal06@gmail.com>
2020-05-13 21:44:03 +0530

    added react-router-dom and is ready to use

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
 import "./App.css";
+import Landing from "./components/Layout/Landing";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Landing} />
+      </div>
+    </Router>
   );
 }
 
