bb868b11085137d3bd1a556b09ab9b692283fa90
Anirudh <anirudhganwal06@gmail.com>
2020-05-25 21:07:18 +0530

    created __init__ of DataGenerator

---
+++ dataGenerator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from tensorflow import keras
+import numpy as np
+import h5py
+
+
+class DataGenerator(keras.utils.Sequence):
+    
+    def __init__(self, filePath, first, size, batchSize=64, shuffle=True):
+        """
+        Initialization for DataGenerator class
+        """
+        self.filePath = filePath
+        self.first = first
+        self.size = size
+        self.batchSize = batchSize
+        self.shuffle = shuffle
